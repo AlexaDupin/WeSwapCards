612d76c721ac6d1590427c5bde083a21e7997639
Alexa Dupin <alexa.dupin@gmail.com>
2024-11-18 23:57:18 +0100

    Controller Handler

--- back/app/routers/api/v1.js
+++ back/app/routers/api/v1.js
@@ -3,11 +3,11 @@ const userController = require('../../controllers/api/user');
 const reportController = require('../../controllers/api/report');
 const opportunitiesController = require('../../controllers/api/opportunities');
 const explorerCardsController = require('../../controllers/api/explorerCards');
-const menuController = require('../../controllers/api/menu');
+const controllerHandler = require('../../helpers/controllerHandler');
 
 const router = express.Router();
 
-router.post('/register', userController.signUp);
+router.post('/register', controllerHandler(userController.signUp));
 router.post('/register/user', userController.authMiddleware, userController.createUser);
 router.post('/login', userController.login);
 router.post('/login/user', userController.authMiddleware, userController.getUserByUID);
@@ -16,63 +16,38 @@ router.get('/signout', userController.signOut);
 
 router
     .route('/places')
-    .get(reportController.getAllPlaces)
+    .get(controllerHandler(reportController.getAllPlaces))
 
 router
     .route('/cards/:placeId')
-    .get(reportController.getCardsFromPlace);
+    .get(controllerHandler(reportController.getCardsFromPlace));
 
 router
     .route('/cards/:placeId/:explorerId')
-    .get(reportController.getExplorerCardsFromOnePlace);
+    .get(controllerHandler(reportController.getExplorerCardsFromOnePlace));
 
 router
     .route('/cards/:placeId/:explorerId/duplicates')
-    .get(reportController.getDuplicateCards);
+    .get(controllerHandler(reportController.getDuplicateCards));
 
 router
     .route('/report/:explorerId')
-    .post(reportController.addCardsToExplorer);
+    .post(controllerHandler(reportController.addCardsToExplorer));
 
 router
     .route('/opportunities/:explorerId')
-    .get(opportunitiesController.getOpportunities);
+    .get(controllerHandler(opportunitiesController.getOpportunities));
 
 router
     .route('/opportunities/:explorerId/:placeId')
-    .get(opportunitiesController.getCountForOnePlaceForOneExplorer);
+    .get(controllerHandler(opportunitiesController.getCountForOnePlaceForOneExplorer));
 
 router
     .route('/explorercards/:explorerId')
-    .get(explorerCardsController.getExplorerCardsByPlace);
+    .get(controllerHandler(explorerCardsController.getExplorerCardsByPlace));
 
 router
     .route('/explorercards/:explorerId/cards/:cardId/duplicate')
-    .patch(explorerCardsController.editDuplicateStatus);
-
- 
-    
-// OLD ROUTES //
-
-// router.get('/menu', userController.authMiddleware, menuController.getMenu);
-router.get('/menu', menuController.getMenu);
-
-/// TEST
-router.post('/menu', menuController.getMenu);
-// router.get('/menu', userController.authMiddleware, menuController.getMenu);
-
-router.get('/user', userController.authMiddleware, userController.getUser);
-router.post('/user', userController.getUserByUID);
-
-// router.get('/cards/:cardId/:explorerId/duplicate', explorerCardsController.getDuplicateStatus);
-
-// router.get('/opportunities/:placeId/:explorerId', opportunitiesController.getOpportunitiesCountForOnePlaceForOneExplorer);
-
-
-// router.post('/declare', userController.authMiddleware,declareController.addCardsToExplorer);
-// router.post('/opportunities', opportunitiesController.handleOpportunities);
-
-
-
+    .patch(controllerHandler(explorerCardsController.editDuplicateStatus));
 
 module.exports = router;
