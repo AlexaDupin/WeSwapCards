748108a6c98fba6881396f1f1a3b978b1e4571a0
Alexa Dupin <alexa.dupin@gmail.com>
2025-03-06 18:36:58 +0100

    Carousel modal created

---
+++ front/src/components/Carousel/Carousel.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+
+import {
+  Carousel, Modal 
+} from "react-bootstrap";
+import { useNavigate } from 'react-router-dom';
+
+import CustomButton from '../CustomButton/CustomButton';
+
+import Report from '../../images/reportPL.svg';
+import Search from '../../images/searchPL.svg';
+import Users from '../../images/usersPL.svg';
+import Chat from '../../images/chatdealPL.svg';
+import Dashboard from '../../images/dashboardPL.svg';
+import Check from '../../images/checkPL.svg';
+
+import './carouselStyles.scss';
+
+const CustomCarousel = ({  }) => {
+  const [index, setIndex] = useState(0);
+  const [show, setShow] = useState(true);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  return (
+
+    <div
+      className="modal show"
+      style={{ display: 'block', position: 'initial' }}
+    >
+    <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title className="modal-title text-center w-100">Welcome to WeSwapCards!</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+            <Carousel activeIndex={index} onSelect={handleSelect} controls={true} indicators={false} interval={null} className="custom-carousel">
+              <Carousel.Item>
+                <div className="carousel-content text-center">
+                  <img src={Report} alt="Report icon" className="carousel-image" />
+                  <div className="caption-container">
+                    <h3 className="caption-title">Log all your cards</h3>
+                    <p className="caption-text">Go to "Report my cards", log the cards you have and specify if you have them in duplicates.</p>
+                  </div>
+                </div>
+              </Carousel.Item>
+              <Carousel.Item>
+                <div className="carousel-content text-center">
+                  <img src={Search} alt="Search icon" className="carousel-image" />
+                  <div className="caption-container">
+                    <h3 className="caption-title">Search for the cards you need</h3>
+                    <p className="caption-text">Go to "Find a card", select a chapter and a card number and see who has it.</p>
+                  </div>
+                </div>
+              </Carousel.Item>
+              <Carousel.Item>
+                <div className="carousel-content text-center">
+                  <img src={Dashboard} alt="Dashboard icon" className="carousel-image" />
+                  <div className="caption-container">
+                    <h3 className="caption-title">Check all your conversations</h3>
+                    <p className="caption-text">Make sure to regularly go to "View all requests" to see if you have new messages.</p>
+                  </div>
+                </div>
+              </Carousel.Item>
+              <Carousel.Item>
+                <div className="carousel-content text-center">
+                  <img src={Check} alt="Check icon" className="carousel-image" />
+                  <div className="caption-container">
+                    <h3 className="caption-title">Check your cards status</h3>
+                    <p className="caption-text">Go to "Check my cards" to make sure your cards are up-to-date. You can easily update the duplicate status from there.</p>
+                  </div>
+                </div>
+              </Carousel.Item>
+            </Carousel>
+
+        </Modal.Body>
+        </Modal>
+    </div>
+  );
+};
+
+export default CustomCarousel;
--- front/src/components/Menu/Menu.jsx
+++ front/src/components/Menu/Menu.jsx
@@ -6,6 +6,7 @@ import {
     Button
 } from "react-bootstrap";
 import { useNavigate } from 'react-router-dom';
+import CarouselSlides from '../Carousel/Carousel';
 
 import Report from '../../images/reportPL.svg';
 import Search from '../../images/searchPL.svg';
@@ -30,70 +31,75 @@ function Menu({
      }, []);
 
   return (
-    <Container className="page-container">
-    <h1 className="menu-title" style={{fontSize: '1.2rem'}}>
-        Welcome {name}!
-    </h1>
+    <><Container className="page-container">
+          <h1 className="menu-title" style={{ fontSize: '1.2rem' }}>
+              Welcome {name}!
+          </h1>
 
-    <section className="menu-steps">    
+          <section className="menu-steps">
 
-        <section className="menu-image-section">
-            <a href="/report" className="menu-link">
-                <img src={Report} alt="Report icon" className="menu-image"/>
-            </a>
-            <Button
-                onClick={() => navigate('/report')}
-                className="menu-button"
-            >
-            Report my cards
-            </Button>
-        </section>
-        <section className="menu-image-section">
-            <a href="/swap/card" className="menu-link">
-                <img src={Search} alt="Search icon" className="menu-image"/>
-            </a>
-            <Button
-                onClick={() => navigate('/swap/card')}
-                className="menu-button"
-            >
-            Find a card
-            </Button>
-        </section>
-        <section className="menu-image-section">
-            <a href="/check" className="menu-link">
-                <img src={Check} alt="Check icon" className="menu-image"/>
-            </a>
-            <Button
-                onClick={() => navigate('/check')}
-                className="menu-button"
-            >
-            Check my cards
-            </Button>
-        </section>
-        <section className="menu-image-section">
-            <a href="/swap/dashboard" className="menu-link">
-                <img src={Dashboard} alt="Dashboard icon" className="menu-image"/>
-            </a>
-            <Button
-                onClick={() => navigate('/swap/dashboard')}
-                className="menu-button"
-            >
-            View all requests
-            </Button>
-        </section>
-        <section className="btn-legal">
-            <Button
-                onClick={() => navigate('/legal')}
-                className="menu-button"
-            >
-            Legal
-            </Button>
-        </section>
+              <section className="menu-image-section">
+                  <a href="/report" className="menu-link">
+                      <img src={Report} alt="Report icon" className="menu-image" />
+                  </a>
+                  <Button
+                      onClick={() => navigate('/report')}
+                      className="menu-button"
+                  >
+                      Report my cards
+                  </Button>
+              </section>
+              <section className="menu-image-section">
+                  <a href="/swap/card" className="menu-link">
+                      <img src={Search} alt="Search icon" className="menu-image" />
+                  </a>
+                  <Button
+                      onClick={() => navigate('/swap/card')}
+                      className="menu-button"
+                  >
+                      Find a card
+                  </Button>
+              </section>
+              <section className="menu-image-section">
+                  <a href="/check" className="menu-link">
+                      <img src={Check} alt="Check icon" className="menu-image" />
+                  </a>
+                  <Button
+                      onClick={() => navigate('/check')}
+                      className="menu-button"
+                  >
+                      Check my cards
+                  </Button>
+              </section>
+              <section className="menu-image-section">
+                  <a href="/swap/dashboard" className="menu-link">
+                      <img src={Dashboard} alt="Dashboard icon" className="menu-image" />
+                  </a>
+                  <Button
+                      onClick={() => navigate('/swap/dashboard')}
+                      className="menu-button"
+                  >
+                      View all requests
+                  </Button>
+              </section>
+              <section className="btn-legal">
+                  <Button
+                      onClick={() => navigate('/legal')}
+                      className="menu-button"
+                  >
+                      Legal
+                  </Button>
+              </section>
 
-    </section>
+          </section>
 
-    </Container>
-)
+      </Container>
+      
+      <section className="">
+              <CarouselSlides />
+          </section></>
+
+    )
 }
 
 export default React.memo(Menu);
