65d1bc2ef9705221cac5cb60d59c0ec743c1a941
Alexa Dupin <alexa.dupin@gmail.com>
2025-09-22 20:25:27 +0200

    fix(header): fixed glitch on refresh

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
-import { UserButton, useUser } from "@clerk/clerk-react";
+// import { UserButton, useUser } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 
 import {
   NavLink, Link, useNavigate
@@ -12,7 +13,7 @@ import './headerStyles.scss';
 import useStickyVars from '../../hooks/useStickyVars';
 
 function Header() {
-  const { isSignedIn } = useUser();
+  // const { isSignedIn } = useUser();
   const navigate = useNavigate();
 
   const appHeaderRef = useRef(null);
@@ -21,7 +22,44 @@ function Header() {
   return (
     <header ref={appHeaderRef} className="header border-bottom fixed-top">
         
-        {isSignedIn ?
+        <SignedIn>
+        <div>
+          <NavLink to="/menu" style={{ textDecoration: 'none' }}>
+            <h1 className="header-title m-0 header-title-link">WeSwapCards</h1>
+          </NavLink>
+        </div>
+        <nav className="header-nav" id="header-nav-login">
+          <NavDropdown title="Swap" id="basic-nav-dropdown" className="header-nav-item">
+            <NavDropdown.Item as={Link} to="/swap/card">Find a card</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/swap/dashboard">Check all requests</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown.Item
+            title="My cards"
+            id="basic-nav-dropdown"
+            className="header-nav-item mr-3"
+            as={Link}
+            to="/cards"
+          >
+            My cards
+          </NavDropdown.Item>
+          <UserButton afterSignOutUrl="/login" />
+        </nav>
+      </SignedIn>
+
+      <SignedOut>
+        <div style={{ margin: 0 }}>
+          <NavLink to="/" style={{ textDecoration: 'none' }}>
+            <h1 className="header-title m-0 header-title-link">WeSwapCards</h1>
+          </NavLink>
+        </div>
+        <nav className="header-nav header-nav-login">
+          <Button variant="light" className="header-nav-login" onClick={() => navigate('/login')}>
+            Sign in
+          </Button>
+        </nav>
+      </SignedOut>
+
+        {/* {isSignedIn ?
         <div>
           <NavLink to="/menu" style={{ textDecoration: 'none' }}>
             <h1 className="header-title m-0 header-title-link">WeSwapCards</h1>
@@ -59,7 +97,7 @@ function Header() {
         >
           <Button variant="light" className="header-nav-login" onClick={() => navigate('/login')}>Sign in</Button>
         </nav> 
-      }
+      } */}
 
     </header> 
 
