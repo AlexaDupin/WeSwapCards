78a10b8070600bd2884a4e4a9ee6a7841b2c36db
Alexa Dupin <alexa.dupin@gmail.com>
2025-09-23 17:12:32 +0200

    fix(footer): added hover effect

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  NavLink, useLocation
+  NavLink
 } from 'react-router-dom';
 import {
 	Eyeglasses,
@@ -11,7 +11,6 @@ import PropTypes from 'prop-types';
 
 import './footerStyles.scss';
 function Footer() {
-  const location = useLocation();
 
   return (
     <footer className="footer text-center ">
@@ -24,8 +23,8 @@ function Footer() {
           <a href="/terms" target="_blank" className="footer-desktop-link" rel="noopener noreferrer">
                 <p className="footer-desktop-caption">Terms and Conditions</p>
           </a>
-          <a href="/cookies" target="_blank" className="footer-general-link" rel="noopener noreferrer">
-                <p className="footer-general-caption">Cookie Policy</p>
+          <a href="/cookies" target="_blank" className="footer-desktop-link" rel="noopener noreferrer">
+                <p className="footer-desktop-caption">Cookie Policy</p>
           </a>
           <a href="/contact" target="_blank" className="footer-desktop-link" rel="noopener noreferrer">
                 <p className="footer-desktop-caption">Contact us</p>
@@ -34,22 +33,6 @@ function Footer() {
 
       
       {/* For mobile screens */}
-      {location.pathname === "/" || location.pathname === "/register/user" || location.pathname === "/register" || location.pathname === "/login" || location.pathname === "/privacy" || location.pathname === "/cookies" || location.pathname === "/terms" || location.pathname === "/contact" ?
-            <div className="footer-general border-top">
-             <a href="/privacy" target="_blank" className="footer-general-link" rel="noopener noreferrer">
-                <p className="footer-general-caption">Privacy</p>
-             </a>
-             <a href="/terms" target="_blank" className="footer-general-link" rel="noopener noreferrer">
-                <p className="footer-general-caption">Terms</p>
-             </a>
-             <a href="/cookies" target="_blank" className="footer-general-link" rel="noopener noreferrer">
-                <p className="footer-general-caption">Cookies</p>
-             </a>
-             <a href="/contact" target="_blank" className="footer-general-link" rel="noopener noreferrer">
-                <p className="footer-general-caption">Contact</p>
-             </a>
-           </div>
-      :
       <div className="footer-mobile border-top fixed-bottom">
         <NavLink 
           to="/swap/card" 
@@ -78,7 +61,6 @@ function Footer() {
           <p className="footer-mobile-caption">My cards</p>
         </NavLink>
       </div>
-      }
         
     </footer>     
   );
